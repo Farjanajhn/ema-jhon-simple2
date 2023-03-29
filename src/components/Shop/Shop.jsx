c95d8c8ea15ecce8d4a23cd2f5e5910b9b313eef
Farjana <farjanajhn12@gmail.com>
2023-03-29 13:06:57 +0200

    saved cart to local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch('products.json')
       .then(res => res.json())
-      .then(data=>setProducts(data))
-  }, [])
-  const [cart, setCart] = useState([]);
+      .then(data => setProducts(data))
+  }, []);
+
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    console.log('17 storedCart', storedCart);
+  },[])
+
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id)
   }
   return (
     <div className='shop-container'>
